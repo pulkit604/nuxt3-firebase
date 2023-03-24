321dd92d26ad9cd8fd5cf0374e9ce36eaa6a67cf
pulkit604 <guppul@gmail.com>
2023-03-24 12:45:02 +0100

    Update index.vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -7,7 +7,7 @@
         <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
           <span class="block xl:inline">Data to enrich your</span>
           {{ ' ' }}
-          <span class="block text-indigo-600 xl:inline">online business</span>
+          <span class="block text-indigo-600 xl:inline">online test business</span>
         </h1>
         <p class="mx-auto mt-3 max-w-md text-base text-gray-500 sm:text-lg md:mt-5 md:max-w-3xl md:text-xl">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
         <div class="mx-auto mt-5 max-w-md sm:flex sm:justify-center md:mt-8">
